594ce4ee8e900e6580236ad910d2015789febb94
Darnxca <carminedac@gmail.com>
2023-01-05 17:42:41 +0100

    implmentata lettura dei file con estensione NewLang (nl) + correzion bug mancata scrittura del tipo per 'var'

---
+++ CodiciC/TestWhile.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+// prototipi delle funzioni
+int main();
+
+// inizializzazione delle variabili
+
+//-----------Implementazione funzioni-----------
+int main ( ){
+
+// Dichiarazione variabili
+int i = 0;
+while((((i + 5) < 2) && ((i * 9) > 2))){
+
+// Dichiarazione variabili
+int x = 0;
+while((1 ||  !((i + 3) < 0))){
+
+// Dichiarazione variabili
+int z = 3;
+int vero = 0;
+while((((x + 2) > 3) &&  !vero)){
+
+// Dichiarazione variabili
+vero = (1 && 0);
+if((vero || 1)){
+
+// Dichiarazione variabili
+char*  helloWorld = strcpy(malloc(13*sizeof(char)),"Hello world!");
+char*  nome = strcpy(malloc(14*sizeof(char)),"Lionel Messi!");
+printf("%s\n",strcat(helloWorld,nome));
+fflush(stdout);
+}
+}
+}
+}
+}
