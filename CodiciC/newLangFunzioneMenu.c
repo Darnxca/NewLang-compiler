30741dd9b8cef6a16370b3b6decf54b3bc5e3e40
Darnxca <carminedac@gmail.com>
2023-01-11 18:26:06 +0100

    Inserimento Documentazione_NewLang.pdf

--- CodiciC/newLangFunzioneMenu.c
+++ CodiciC/newLangFunzioneMenu.c
@@ -21,6 +21,9 @@ void initialize_global() {
 
 //-----------Implementazione funzioni-----------
 void stampaMenu ( ){
+
+
+	// Inizio Statement
 	printf("%s\n","Che operazione vuoi eseguire");
 	fflush(stdout);
 	printf("%s\n","2 - Somma");
@@ -31,6 +34,7 @@ void stampaMenu ( ){
 	fflush(stdout);
 	printf("%s\n","5 - Divisione");
 	fflush(stdout);
+
 }
 float somma (float a, float b, float *result ){
 	// Dichiarazione variabili
@@ -40,26 +44,50 @@ float somma (float a, float b, float *result ){
 	int x;
 	float ez;
 	float l;
+	// Assegnazione variabili
 	plal = 2;
 	x = 3;
+
+	// Inizio Statement
 	*result = (a + b);
 	return 2.2;
+
 }
 void sottrai (float a, float b, float *result ){
+
+
+	// Inizio Statement
 	*result = (a - b);
+
 }
 void moltiplica (float a, float b, float *result ){
+
+
+	// Inizio Statement
 	*result = (a * b);
+
 }
 int dividi (float a, float b, float *result ){
+
+
+	// Inizio Statement
 	if((b == 0)){
+
+
+		// Inizio Statement
 		*result = 0;
 		return 0;
+
 	}
 	else{
+
+
+		// Inizio Statement
 		*result = (a / b);
 		return 1;
+
 	}
+
 }
 int main ( ){
 	initialize_global();
@@ -71,16 +99,22 @@ int main ( ){
 	int sceltaOp;
 	char*  ciao;
 	float result;
+	// Assegnazione variabili
 	continuaScelta = 1;
 	num1 = 0;
 	num2 = 0.0;
 	ciao = "CISIDIIDI";
+
 	casa = (2 + (2 + (2.2 * (2 / ( -1)))));
+	// Inizio Statement
 	stampaMenu();
 	printf("Scegli l'operazione da effettuare\n");
 	fflush(stdout);
 	scanf("%d", &sceltaOp);
 	while(((continuaScelta != 0) && ((sceltaOp > 1) && (sceltaOp < 6)))){
+
+
+		// Inizio Statement
 		printf("Inserisci il primo numero\n");
 		fflush(stdout);
 		scanf("%f", &num1);
@@ -88,19 +122,39 @@ int main ( ){
 		fflush(stdout);
 		scanf("%f", &num2);
 		if((sceltaOp == 2)){
+
+
+			// Inizio Statement
 			somma(num1, num2, &result);
+
 		}
 		if((sceltaOp == 3)){
+
+
+			// Inizio Statement
 			sottrai(num1, num2, &result);
+
 		}
 		if((sceltaOp == 4)){
+
+
+			// Inizio Statement
 			moltiplica(num1, num2, &result);
+
 		}
 		if((sceltaOp == 5)){
+
+
+			// Inizio Statement
 			if(( !dividi(num1, num2, &result))){
+
+
+				// Inizio Statement
 				printf("%s","Errore operazione di divisione non effettuabile!!!");
 				fflush(stdout);
+
 			}
+
 		}
 		printf("%s %f %f %s %f","Il risultato dell'operazione scelta tra ", num1, num2, "è", result);
 		fflush(stdout);
@@ -108,15 +162,25 @@ int main ( ){
 		fflush(stdout);
 		scanf("%d", &continuaScelta);
 		while(((continuaScelta != 0) && (continuaScelta != 1))){
+
+
+			// Inizio Statement
 			printf("Inserire 0 per terminare, 1 per continuare!\n");
 			fflush(stdout);
 			scanf("%d", &continuaScelta);
+
 		}
 		if((continuaScelta == 1)){
+
+
+			// Inizio Statement
 			stampaMenu();
 			printf("Scegli loperazione da effettuare\n");
 			fflush(stdout);
 			scanf("%d", &sceltaOp);
+
 		}
+
 	}
+
 }
