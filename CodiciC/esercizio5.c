f4d70c9e4f4c2803718e995af69920e1596f7db7
darnxca <c.dangelo23@studenti.unisa.it>
2023-01-24 17:35:23 +0100

    Correzzione bug script bash

--- CodiciC/esercizio5.c
+++ CodiciC/esercizio5.c
@@ -10,18 +10,25 @@ void stampaSequenzaFibonacci(int n);
 int Nfibonacci(int n);
 void main();
 int dividi_interi(int a,int b,int *result);
+// inizializzazione delle variabili globali
 
-// inizializzazione delle variabili
+void initialize_global() {
+}
 
 //-----------Implementazione funzioni-----------
 void prodotto_tramite_addizione ( ){
 	// Dichiarazione variabili
 	float num1;
+	float totale;
 	float num2;
 	float tmp1;
 	float tmp2;
-	float totale = 0;
-	int i = 0;
+	int i;
+	// Assegnazione variabili
+	totale = 0;
+	i = 0;
+
+	// Inizio Statement
 	printf("Inserisci primo numero: \n");
 	fflush(stdout);
 	scanf("%f", &num1);
@@ -29,48 +36,78 @@ void prodotto_tramite_addizione ( ){
 	fflush(stdout);
 	scanf("%f", &num2);
 	if(((num1 < 0) && (num2 < 0))){
-		// Dichiarazione variabili
+
+
+		// Inizio Statement
 		tmp1 = (num1 * ( -1));
 		tmp2 = (num2 * ( -1));
+
 	}
 	else{
-		// Dichiarazione variabili
+
+
+		// Inizio Statement
 		tmp1 = num1;
 		tmp2 = num2;
+
 	}
 	if((tmp2 >= 0)){
-		// Dichiarazione variabili
+
+
+		// Inizio Statement
 		while((i < tmp2)){
-			// Dichiarazione variabili
+
+
+			// Inizio Statement
 			totale = (totale + tmp1);
 			i = (i + 1);
+
 		}
+
 	}
 	else{
-		// Dichiarazione variabili
+
+
+		// Inizio Statement
 		while((i < tmp1)){
-			// Dichiarazione variabili
+
+
+			// Inizio Statement
 			totale = (totale + tmp2);
 			i = (i + 1);
+
 		}
+
 	}
 	printf("%s %f %s %f %s %f\n","Il prodotto tra ", num1, "e", num2, " è ", totale);
 	fflush(stdout);
+
 }
 int Nfibonacci (int n ){
-	// Dichiarazione variabili
+
+
+	// Inizio Statement
 	if((n == 0)){
-		// Dichiarazione variabili
+
+
+		// Inizio Statement
 		return 0;
+
 	}
 	if((n == 1)){
-		// Dichiarazione variabili
+
+
+		// Inizio Statement
 		return 1;
+
 	}
 	return (Nfibonacci((n - 1)) + Nfibonacci((n - 2)));
+
 }
 void stampaMenu ( ){
-	// Dichiarazione variabili
+
+
+	// Inizio Statement
 	printf("%s\n","Che operazione vuoi eseguire");
 	fflush(stdout);
 	printf("%s\n","1 - Somma di due numeri");
@@ -83,45 +120,74 @@ void stampaMenu ( ){
 	fflush(stdout);
 	printf("%s\n","5 - successione di fibonacci");
 	fflush(stdout);
+
 }
-int dividi_interi (int a, int b, int * result ){
-	// Dichiarazione variabili
+int dividi_interi (int a, int b, int *result ){
+
+
+	// Inizio Statement
 	if((b == 0)){
-		// Dichiarazione variabili
-		* result = 0;
+
+
+		// Inizio Statement
+		*result = 0;
 		return 0;
+
 	}
 	else{
-		// Dichiarazione variabili
-		* result = (a / b);
+
+
+		// Inizio Statement
+		*result = (a / b);
 		return 1;
+
 	}
+
 }
 void stampaSequenzaFibonacci (int n ){
 	// Dichiarazione variabili
-	int i = 0;
+	int i;
+	// Assegnazione variabili
+	i = 0;
+
+	// Inizio Statement
 	while((i < n)){
-		// Dichiarazione variabili
+
+
+		// Inizio Statement
 		printf("%d %s %d\n",i, " -> ", Nfibonacci(i));
 		fflush(stdout);
 		i = (i + 1);
+
 	}
+
 }
 void main ( ){
+	initialize_global();
 	// Dichiarazione variabili
+	int continuaScelta;
+	float num1;
+	float num2;
 	int sceltaOp;
 	float result;
-	float num1 = 0;
-	int continuaScelta = 1;
-	float num2 = 0.0;
+	// Assegnazione variabili
+	num1 = 0;
+	continuaScelta = 1;
+	num2 = 0.0;
+
+	// Inizio Statement
 	stampaMenu();
 	printf("Scegli l'operazione da effettuare\n");
 	fflush(stdout);
 	scanf("%d", &sceltaOp);
 	while((((continuaScelta != 0) && (sceltaOp >= 1)) && (sceltaOp <= 5))){
-		// Dichiarazione variabili
+
+
+		// Inizio Statement
 		if((sceltaOp == 1)){
-			// Dichiarazione variabili
+
+
+			// Inizio Statement
 			printf("Inserisci il primo numero\n");
 			fflush(stdout);
 			scanf("%f", &num1);
@@ -130,14 +196,21 @@ void main ( ){
 			scanf("%f", &num2);
 			printf("%s %f %s %f %s %f\n","La somma tra ", num1, " e ", num2, " e' ", (num1 + num2));
 			fflush(stdout);
+
 		}
 		if((sceltaOp == 2)){
-			// Dichiarazione variabili
+
+
+			// Inizio Statement
 			prodotto_tramite_addizione();
+
 		}
 		if((sceltaOp == 3)){
 			// Dichiarazione variabili
 			int result;
+			// Assegnazione variabili
+
+			// Inizio Statement
 			printf("Inserisci il primo numero\n");
 			fflush(stdout);
 			scanf("%f", &num1);
@@ -145,28 +218,44 @@ void main ( ){
 			fflush(stdout);
 			scanf("%f", &num2);
 			if(((num1 >= 0) && (num2 >= 0))){
-				// Dichiarazione variabili
+
+
+				// Inizio Statement
 				if(dividi_interi(num1, num2, &result)){
-					// Dichiarazione variabili
+
+
+					// Inizio Statement
 					printf("%s %f %s %f %s %d\n","il risultato della divisione tra ", num1, " e ", num2, " ", result);
 					fflush(stdout);
+
 				}
 				else{
-					// Dichiarazione variabili
+
+
+					// Inizio Statement
 					printf("%s\n","Attenzione denominatore uguale a 0");
 					fflush(stdout);
+
 				}
+
 			}
 			else{
-				// Dichiarazione variabili
+
+
+				// Inizio Statement
 				printf("%s\n","Errore uno dei due numeri inseriti non è un intero positivo");
 				fflush(stdout);
+
 			}
+
 		}
 		if((sceltaOp == 4)){
 			// Dichiarazione variabili
 			float num1;
 			float esponente;
+			// Assegnazione variabili
+
+			// Inizio Statement
 			printf("Inserisci numero\n");
 			fflush(stdout);
 			scanf("%f", &num1);
@@ -175,43 +264,62 @@ void main ( ){
 			scanf("%f", &esponente);
 			printf("%f %s %f %s %f\n",num1, "^", esponente, " e' ", pow(num1,esponente));
 			fflush(stdout);
+
 		}
 		if((sceltaOp == 5)){
 			// Dichiarazione variabili
 			int num2;
 			int esponente;
+			// Assegnazione variabili
+
+			// Inizio Statement
 			printf("Inserisci quanti numeri della sequenza di fibonacci visualizzare\n");
 			fflush(stdout);
 			scanf("%d", &num2);
 			if((num2 <= 0)){
-				// Dichiarazione variabili
+
+
+				// Inizio Statement
 				printf("%s\n","Errore!: numero inserito negativo oppure uguale a 0");
 				fflush(stdout);
+
 			}
 			else{
-				// Dichiarazione variabili
+
+
+				// Inizio Statement
 				printf("%s %d %s\n","La sequenza di fibonacci per ", num2, " e':");
 				fflush(stdout);
 				stampaSequenzaFibonacci(num2);
+
 			}
+
 		}
 		printf("Vuoi continuare? (0: NO, 1: SI)\n");
 		fflush(stdout);
 		scanf("%d", &continuaScelta);
 		while(((continuaScelta != 0) && (continuaScelta != 1))){
-			// Dichiarazione variabili
+
+
+			// Inizio Statement
 			printf("Inserire 0 per terminare, 1 per continuare!\n");
 			fflush(stdout);
 			scanf("%d", &continuaScelta);
+
 		}
 		if((continuaScelta == 1)){
-			// Dichiarazione variabili
+
+
+			// Inizio Statement
 			stampaMenu();
 			printf("Scegli loperazione da effettuare\n");
 			fflush(stdout);
 			scanf("%d", &sceltaOp);
+
 		}
+
 	}
 	printf("%s\n","Il programma è terminato!!!");
 	fflush(stdout);
+
 }
